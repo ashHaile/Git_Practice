a5bd2ffbde8dbd4da6332dcfb5470e7ddc510ebc
AshHaile <ashuhaile08@gmail.com>
2020-08-19 15:44:47 -0700

    Add Test3 & Test4

---
+++ src/day1/Note_Git_Hub_shortCut.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class Note_Git_Hub_shortCut {
+    /*
+
+    08/18/2020
+Practice Topic: IntelliJ Short cuts
+                Git Short cuts
+Task:
+    1. Make sure you know your github username and password
+    2. make sure you already know the definition of basic terminal commands of git
+    git init ==> VCS -> Enable version control system intergration
+    git add .
+    git add fileName
+    git commit -m
+    git push
+    git clone
+    git pull
+    fetch
+
+    fork*/
+}
--- src/day2/Test2.java
+++ src/day2/Test2.java
@@ -1,4 +1,14 @@
 package day2;
 
 public class Test2 {
+    public static void main(String[] args) {
+        String result = "";
+        int count = 2;
+        for (int i = 1; i <= 9 ; i++) {
+            result += i;
+            System.out.println(result + " * "+ 9+" + "+count+" = "+(Integer.parseInt(result)*9+count));
+            count++;
+        }
+    }
 }
+
